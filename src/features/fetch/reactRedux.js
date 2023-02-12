5b4b73b495e11489e9b9f8d2fc8c78da715b094e
dev4ult <nibrasbiasaaja@gmail.com>
2023-02-12 12:44:42 +0700

    minor changes

--- src/features/fetch/reactRedux.js
+++ src/features/fetch/reactRedux.js
@@ -2,26 +2,34 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import initialState from './initialState';
 
-export const getWeather = createAsyncThunk('get/getWeather', async () => {
-  return fetch(`https://api.timezonedb.com/v2.1/list-time-zone?key=${import.meta.env.VITE_API_KEY}&format=json`).then((res) => res.json());
+export const getWeather = createAsyncThunk('get/getWeather', async (data, thunkApi) => {
+  try {
+    const response = await axios(`https://api.timezonesdb.com/v2.1/list-time-zone?key=${import.meta.env.VITE_API_KEY}&format=json`);
+
+    return response.data;
+  } catch (err) {
+    const message = err.message;
+    return thunkApi.rejectWithValue(message);
+  }
 });
 
 const fetchSlice = createSlice({
   name: 'fetch',
   initialState,
 
-  extraReducers: {
-    [getWeather.pending]: (state) => {
-      state.loading = true;
-    },
-    [getWeather.fulfilled]: (state, action) => {
-      state.weatherList = action.payload;
-      state.loading = false;
-    },
-    [getWeather.rejected]: (state, action) => {
-      state.error = action.payload;
-      state.loading = false;
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getWeather.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getWeather.fulfilled, (state, action) => {
+        state.weatherList = action.payload;
+        state.loading = false;
+      })
+      .addCase(getWeather.rejected, (state, action) => {
+        state.error = action.payload;
+        state.loading = false;
+      });
   },
 });
 
